a04f33ec3b0cad70c27796c4e18f83c309357982
Akash Kumar <akashyadav112314@gmail.com>
2022-08-27 23:39:41 +0530

    figured out why [[ ]]; was required
    = operator and more about strings in general

--- user_input/authorization.sh
+++ user_input/authorization.sh
@@ -2,9 +2,13 @@
 
 read -p "Username: " username
 read -sp "Password: " password
-# if statement doesn't work with single [ ] brackets
-# [[ ]]; is required for it to work
-if [[ $username == akashkumar && $password == nothing ]];
+
+# every conditional statement is surrounded with [ ] with spaces, otherwise it doesn't work
+# [ $username = akashkumar] is a conditional statement and that is surrounded by [ ]; of if clause making double [ ]
+# = operator is used to check for string equality instead of ==
+# TODO
+# -a is AND operator yet it doesn't seem to work in place of &&
+if [[ $username = akashkumar && $password = nothing ]];
 then
     echo -e "\nlogged in"
 else
